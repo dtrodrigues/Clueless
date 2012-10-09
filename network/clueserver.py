e9341fef839e6303106bdf71e6b515341e42dc93
Guy Bernal <guybernal@gmail.com>
2012-10-09 14:40:57 -0400

    Six players can now join a game, added stubs and logging for message passing

--- network/clueserver.py
+++ network/clueserver.py
@@ -6,49 +6,81 @@ from twisted.application import service
 
 from messageprotocol import MessageReceiver
 
-
+cluePlayers = ['Scarlett',
+               'Mustard',
+               'White',
+               'Green',
+               'Peacock',
+               'Plum']
+               
 class GameServerProtocol(MessageReceiver):
 
     def __init__(self):
-        self.game = None
-        self.opponent = None
+        self.playerName = None
+        self.players = None
 
     def connectionMade(self):
         peer = self.transport.getPeer()
         log.msg("Connection made from {0}:{1}".format(peer.host, peer.port))
 
-        # Find an opponent or add self to a queue
-        self.factory.findOpponent(self)
+        self.factory.findOpponents(self)
 
     def connectionLost(self, reason):
         peer = self.transport.getPeer()
         log.msg("Connection lost from {0}:{1}".format(peer.host, peer.port))
         self.factory.playerDisconnected(self)
 
+    def assignPlayer(self, assignedName):
+        self.playerName = assignedName
+        
+    def startGame(self, playerMap):
+        self.players = playerMap
+        
     def messageReceived(self, message):
-        """Decodes and runs a command from the received data"""
-        log.msg('Data received: {0}'.format(message))
-        self.sendMessage(message)
+        """Called whenever a message is received from a client"""
+        peer = self.transport.getPeer()
+        log.msg('Message received from {0}:{1}:{2}'.format(self.playerName, 
+                                                           peer.host, 
+                                                           peer.port))
+        for character in self.players:
+            if character != self.playerName:
+                self.players[character].sendMessage(message)
 
 
 class GameFactory(protocol.ServerFactory):
 
     protocol = GameServerProtocol
-    queue = deque()
+
 
     def __init__(self, service):
         self.service = service
-
+        self.availablePlayers = cluePlayers
+        self.assignedPlayers = {}
+        
     def findOpponents(self, player):
-        try:
-            opponent = self.queue.popleft()
-        except IndexError:
-            self.queue.append(player)
-        else:
-            game = Game()
-            player.startGame(game, opponent, side1)
-            opponent.startGame(game, player, side2)
+    
+        assignedPlayer = self.availablePlayers.pop()
+        peer = player.transport.getPeer()
+        log.msg('{0}:{1} assigned to {2}'.format(peer.host, peer.port, 
+                                                 assignedPlayer))
+        self.assignedPlayers[assignedPlayer] = player
+        player.assignPlayer(assignedPlayer)
+        
+        #wait for other players
+            
+        if self.availablePlayers == []:
+            #six players joined so start game
+            log.msg('Six players joined, starting game')
+            for player in self.assignedPlayers.values():
+                player.startGame(self.assignedPlayers)
+            
+            #reset
+            self.assignedPlayers = {}
+            self.availablePlayers = cluePlayers
+            
+            #game started
 
+            
     def playerDisconnected(self, player):
         try:
             self.queue.remove(player)
