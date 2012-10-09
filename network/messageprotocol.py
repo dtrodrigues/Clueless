e7a87662434bd57f39fc999cce9203288b302f98
Guy Bernal <guybernal@gmail.com>
2012-10-09 07:07:23 -0400

    removed unnecessary code from template, removed game.py references, added message passing interface, changed protocol from json to pickle

---
+++ network/messageprotocol.py
@@ -0,0 +1,15 @@
+import simplejson as json
+from twisted.protocols import basic
+import pickle
+
+class MessageReceiver(basic.LineOnlyReceiver):
+
+    def lineReceived(self, message):
+        self.messageReceived(pickle.loads(message))
+
+    def messageReceived(self, message):
+        raise NotImplementedError
+
+    def sendMessage(self, message):
+        self.sendLine(pickle.dumps(message))
+
